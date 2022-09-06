2d936240e81f9d8ea428ada7373d0275f0446106
Dvuglaf <aizeensan@gmail.com>
2022-09-06 23:04:42 +0400

    first lab done. only necessary part

--- lab_1_simple_methods/main.py
+++ lab_1_simple_methods/main.py
@@ -1,13 +1,17 @@
-# Задание (встраиваем в зеленый)
-# Номера НЗБП в СВИ-1 (𝑝) с указанием цветовых каналов:
-# Blue-4 XOR Green-1
-# Цветовой канал, используемый в СВИ-4:
-# Cb
-import copy
+# Лабораторная работа 1.
+# Вариант 24
+# Номера НЗБП в СВИ-1 (𝑝) с указанием цветовых каналов: Blue-4 XOR Green-1 (встраиваем в зеленый: 232 vs 150)
+# Цветовой канал, используемый в СВИ-4: Cb
+# Способ встраивания в СВИ-4: (3.13), add = С (mod delta)
 
+import copy
+import numpy as np
 from skimage.io import imread, imshow, show
 from matplotlib import pyplot as plt
-import numpy as np
+
+
+VAR = 24
+DELTA = (4 + 4 * VAR) % 3
 
 
 def read_image(image_path):
@@ -18,7 +22,7 @@ def get_channels(image):
     return image[:, :, 0], image[:, :, 1], image[:, :, 2]
 
 
-def get_layer(channel, idx):
+def get_bit_layer(channel, idx):
     # 1. Занулить все биты, кроме нужной плоскости (&)
     # 2. Привести к бинарной матрице (сдвиг)
     return (channel & (2 ** (idx - 1))) >> (idx - 1)
@@ -28,11 +32,11 @@ def embedding_svi_1(image, watermark):
     copy_image = copy.copy(image)
 
     _, green, blue = get_channels(image)
-    virtual_layer = get_layer(blue, 4) ^ get_layer(green, 1)  # "виртуальная" битовая плоскость
+    virtual_layer = get_bit_layer(blue, 4) ^ get_bit_layer(green, 1)  # "виртуальная" битовая плоскость
 
-    embedded_layer = virtual_layer ^ watermark  # встраиваемая битовая плоскость
+    embedded_layer = virtual_layer ^ watermark  # встраиваемая битовая плоскость (3.4)
 
-    new_green = (green & 254) | embedded_layer  # занулить первую битовую плоскость и встроить плоскость
+    new_green = (green & 254) | embedded_layer  # занулить первую битовую плоскость и встроить embedded_layer
     # new_green = (green & 254) | (embedded_layer ^ get_layer(green, 1))
 
     copy_image[:, :, 1] = new_green
@@ -40,23 +44,28 @@ def embedding_svi_1(image, watermark):
     return copy_image
 
 
-def extract_svi_2(image, marked_image):
+def extract_svi_1(image, marked_image):
     _, green, blue = get_channels(image)
     _, marked_green, _ = get_channels(marked_image)
-    return get_layer(blue, 4) ^ get_layer(green, 1) ^ get_layer(marked_green, 1)
+    return get_bit_layer(blue, 4) ^ get_bit_layer(green, 1) ^ get_bit_layer(marked_green, 1)
 
 
-if __name__ == "__main__":
-    image = read_image('./images/baboon.tif')
+def embedding_svi_4(image, watermark):
     copy_image = copy.copy(image)
-    watermark = read_image('./images/mickey.tif') // 255  # (0, 255) -> (0, 1)
+    additive = image[:, :, 1] % DELTA
 
-    marked_image = embedding_svi_1(image, watermark)
-    extracted_watermark = extract_svi_2(image, marked_image)
+    new_cb = (image // (2 * DELTA) * (2 * DELTA))[:, :, 1] + watermark * DELTA + additive
+    copy_image[:, :, 1] = new_cb
+    return copy_image
 
-    _, green, _ = get_channels(image)
-    _, marked_green, _ = get_channels(marked_image)
 
+def extracting_svi_4(image, marked_image):
+    additive = image[:, :, 1] % DELTA
+
+    return (marked_image[:, :, 1] - additive - (image // (2 * DELTA) * (2 * DELTA))[:, :, 1]) / DELTA
+
+
+def plot(image, marked_image, watermark, extracted_watermark, channel, marked_channel, channel_name):
     fig = plt.figure()
     sp = fig.add_subplot(2, 3, 1)
     sp.set_title("Исходное изображение")
@@ -71,11 +80,66 @@ if __name__ == "__main__":
     sp.set_title("Извлеченный ЦВЗ")
     imshow(extracted_watermark, cmap='gray')
     sp = fig.add_subplot(2, 3, 3)
-    sp.set_title("Зеленый канал исходного")
-    imshow(green)
+    sp.set_title(f"{channel_name} канал исходного")
+    imshow(channel)
     sp = fig.add_subplot(2, 3, 6)
-    sp.set_title("Зеленый канал с ЦВЗ")
-    imshow(marked_green)
+    sp.set_title(f"{channel_name} канал с ЦВЗ")
+    imshow(marked_channel)
+
+
+def rgb2ycbcr(image):
+    y = (77 / 256) * image[:, :, 0] + (150 / 256) * image[:, :, 1] + (29 / 256) * image[:, :, 2]
+    cb = image[:, :, 2] - y
+    cr = image[:, :, 0] - y
+
+    ycbcr_image = np.zeros((512, 512, 3))
+    ycbcr_image[:, :, 0] = y
+    ycbcr_image[:, :, 1] = cb
+    ycbcr_image[:, :, 2] = cr
+
+    return ycbcr_image
+
+
+def ycbcr2rgb(image):
+    red = image[:, :, 2] + image[:, :, 0]
+    blue = image[:, :, 1] + image[:, :, 0]
+    green = (image[:, :, 0] - (77 / 256) * red - (29 / 256) * blue) * (256 / 150)
+
+    rgb_image = np.zeros((512, 512, 3))
+
+    rgb_image[:, :, 0] = red
+    rgb_image[:, :, 1] = green
+    rgb_image[:, :, 2] = blue
+    return rgb_image
+
+
+def main():
+    image = read_image('./images/baboon.tif')
+    watermark = read_image('./images/mickey.tif') // 255  # (0, 255) -> (0, 1)
+
+    # СВИ-1
+    marked_image = embedding_svi_1(image, watermark)
+    extracted_watermark = extract_svi_1(image, marked_image)
+
+    _, green, _ = get_channels(image)
+    _, marked_green, _ = get_channels(marked_image)
+
+    plot(image, marked_image, watermark, extracted_watermark, green, marked_green, "Зеленый")
+
+    # СВИ-4
+    converted_image = rgb2ycbcr(image)
+
+    marked_image = embedding_svi_4(converted_image, watermark)
+    extracted_watermark = extracting_svi_4(converted_image, marked_image)
+
+    _, cb, _ = get_channels(converted_image)
+    _, marked_cb, _ = get_channels(marked_image)
+
+    converted_image = ycbcr2rgb(converted_image).astype(np.int32)
+    marked_image = ycbcr2rgb(marked_image).astype(np.int32)
+    plot(converted_image, marked_image, watermark, extracted_watermark, cb, marked_cb, "Cb")
 
     show()
 
+
+main()
