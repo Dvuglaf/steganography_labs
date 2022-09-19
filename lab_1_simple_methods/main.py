17e8c275797d03f6bdcb07638c4b45e0bff8435e
Dvuglaf <aizeensan@gmail.com>
2022-09-20 00:02:30 +0400

    print max error value, add generate_coordinates function

--- lab_1_simple_methods/main.py
+++ lab_1_simple_methods/main.py
@@ -11,7 +11,7 @@ from skimage.io import imread, imshow, show
 from matplotlib import pyplot as plt
 
 VAR = 24
-DELTA = (4 + 4 * VAR) % 3
+DELTA = 4 + 4 * (VAR % 3)
 IMAGE_SHAPE = (512, 512, 3)
 WATERMARK_SHAPE = (512, 512)
 
@@ -31,13 +31,18 @@ def get_bit_layer(channel, idx: int):
     return (channel & (2 ** (idx - 1))) >> (idx - 1)
 
 
-# Преобразование битового вектора в матрицу со случайными координатами
-def bit_vector_to_watermark(bit_vector, seed):
+# Генерация с seed'ом заданного количества уникальных координат
+def generate_coordinates(seed, count):
     rng = np.random.default_rng(seed)  # создание нового битового генератора с seed'ом
 
     # Генерация массива без повторений и получение координат (остаток от деления, целая часть)
-    unique_numbers = rng.choice(IMAGE_SHAPE[0] * IMAGE_SHAPE[1], size=len(bit_vector), replace=False)
-    coordinates = np.array((unique_numbers % IMAGE_SHAPE[0], unique_numbers // IMAGE_SHAPE[0])).T
+    unique_numbers = rng.choice(IMAGE_SHAPE[0] * IMAGE_SHAPE[1], size=count, replace=False)
+    return np.array((unique_numbers % IMAGE_SHAPE[0], unique_numbers // IMAGE_SHAPE[0])).T
+
+
+# Преобразование битового вектора в матрицу со случайными координатами
+def bit_vector_to_watermark(bit_vector, seed):
+    coordinates = generate_coordinates(seed, len(bit_vector))
 
     # Преобразование битового вектора в матрицу на основе сгенерированных координат
     watermark = np.zeros(WATERMARK_SHAPE, dtype=np.uint8)
@@ -71,22 +76,18 @@ def extracting_svi_1(image, marked_image):
     return get_bit_layer(blue, 4) ^ get_bit_layer(green, 1) ^ get_bit_layer(marked_green, 1)
 
 
-# Стеганографическое НЗБ-встраивание
+# Стеганографическое НЗБ-встраивание: key - длина встраиваемой строки, key * 8 - значение seed'а для генератора
 def embedding_svi_2(image, text: str, key: int):
     bit_vector = bitarray.bitarray()
     bit_vector.frombytes(text.encode('utf-8'))
 
     q = len(bit_vector) / (IMAGE_SHAPE[0] * IMAGE_SHAPE[1])  # заполненность контейнера
-    print(f"q: {q}")
+    print(f"\tq: {q}")
 
     if q > 1:
         raise ValueError("Container overflow, string is too big!")
 
-    rng = np.random.default_rng(key)  # создание нового битового генератора с seed'ом
-
-    # Генерация массива без повторений и получение координат (остаток от деления, целая часть)
-    unique_numbers = rng.choice(IMAGE_SHAPE[0] * IMAGE_SHAPE[1], size=len(bit_vector), replace=False)
-    coordinates = np.array((unique_numbers % IMAGE_SHAPE[0], unique_numbers // IMAGE_SHAPE[0])).T
+    coordinates = generate_coordinates(key, len(bit_vector))
 
     copy_image = copy.copy(image)
 
@@ -94,6 +95,7 @@ def embedding_svi_2(image, text: str, key: int):
 
     embedding_layer = get_bit_layer(green, 1)
 
+    # Встраивание в сгенерированные координаты
     for i in range(len(coordinates)):
         embedding_layer[coordinates[i][0], coordinates[i][1]] = bit_vector[i]
 
@@ -121,31 +123,28 @@ def extracting_svi_2(marked_image, key: int):
     return extracted_bit_vector.tobytes().decode('utf-8')  # перевод в строку
 
 
-# Стеганографическое НЗБ-встраивание с данными из задания 1-2
+# Стеганографическое НЗБ-встраивание с данными из задания 1-2: key - длина встраиваемой строки,
+#                                                              key * 8 - значение seed'а для генератора
 def embedding_svi_2_virtual(image, text: str, key: int):
     bit_vector = bitarray.bitarray()
     bit_vector.frombytes(text.encode('utf-8'))
 
     q = len(bit_vector) / (IMAGE_SHAPE[0] * IMAGE_SHAPE[1])  # заполненность контейнера
-    print(f"q: {q}")
+    print(f"\tq: {q}")
 
     if q > 1:
         raise ValueError("Container overflow, string is too big!")
 
     watermark = bit_vector_to_watermark(bit_vector, key)
-
     return embedding_svi_1(image, watermark), watermark
 
 
 def extracting_svi_2_virtual(image, marked_image, key: int):
     extracted_watermark = extracting_svi_1(image, marked_image)
 
-    rng = np.random.default_rng(key)  # создание нового битового генератора с seed'ом
     extracted_bit_vector = bitarray.bitarray(key * 8)
 
-    # Генерация массива без повторений и получение координат (остаток от деления, целая часть)
-    unique_numbers = rng.choice(IMAGE_SHAPE[0] * IMAGE_SHAPE[1], size=key * 8, replace=False)
-    coordinates = np.array((unique_numbers % IMAGE_SHAPE[0], unique_numbers // IMAGE_SHAPE[0])).T
+    coordinates = generate_coordinates(key, key * 8)
 
     # Извлечение
     for i in range(key * 8):
@@ -197,6 +196,8 @@ def plot(title, image, marked_image, watermark, extracted_watermark, channel, ma
     sp.set_title(f"{channel_name} канал с ЦВЗ")
     imshow(marked_channel)
 
+    print(f"\tМаксимальное значение ошибки в канале: {np.max(np.abs(marked_channel - channel))}\n")
+
 
 # Перевод RGB -> YCbCr
 def rgb2ycbcr(image):
@@ -228,6 +229,8 @@ def ycbcr2rgb(image):
 
 # СВИ-1
 def task_1(image, watermark):
+    print("Задание 1-2 (СВИ-1):")
+
     marked_image = embedding_svi_1(image, watermark)
     extracted_watermark = extracting_svi_1(image, marked_image)
 
@@ -239,6 +242,8 @@ def task_1(image, watermark):
 
 # СВИ-4
 def task_2(image, watermark):
+    print("Задание 3-4 (СВИ-4):")
+
     converted_image = rgb2ycbcr(image)
 
     marked_image = embedding_svi_4(converted_image, watermark)
@@ -253,6 +258,8 @@ def task_2(image, watermark):
 
 # СВИ-1 и СВИ-4
 def extra_task_3(image, watermark_1, watermark_2):
+    print("Допзадание 1_1:")
+
     marked_image = embedding_svi_1(image, watermark_1)
 
     converted_image = rgb2ycbcr(marked_image)
@@ -269,13 +276,15 @@ def extra_task_3(image, watermark_1, watermark_2):
     _, green, _ = get_channels(image)
     _, marked_green, _ = get_channels(marked_image)
 
-    plot("Допзадание 1_1", image, marked_image, watermark_2, extracted_watermark_2, cb, marked_cb, "Cb")
-    plot("Допзадание 1_2", image, marked_image, watermark_1, extracted_watermark, green, marked_green, "Зеленый")
+    plot("Допзадание 1_1", image, marked_image, watermark_1, extracted_watermark, green, marked_green, "Зеленый")
+
+    print("Допзадание 1_2:")
+    plot("Допзадание 1_2", image, marked_image, watermark_2, extracted_watermark_2, cb, marked_cb, "Cb")
 
 
-# СВИ-2
+# СВИ-2: key - длина встраиваемой строки, key * 8 - значение seed'а для генератора
 def extra_task_4(image, text):
-    print("СВИ-2:")
+    print("Допзадание 2 (СВИ-2):")
 
     marked_image = embedding_svi_2(image, text, len(text))
     extracted_text = extracting_svi_2(marked_image, len(text))
@@ -289,9 +298,9 @@ def extra_task_4(image, text):
          np.zeros(WATERMARK_SHAPE), green, marked_green, "Зеленый")
 
 
-# СВИ-2 на основании данных из задания 1-2
+# СВИ-2 на основании данных из задания 1-2: key - длина встраиваемой строки, key * 8 - значение seed'а для генератора
 def extra_task_4_virtual(image, text):
-    print("СВИ-2:")
+    print("Допзадание 2 (СВИ-2):")
 
     marked_image, watermark = embedding_svi_2_virtual(image, text, len(text))
     extracted_text, extracted_watermark = extracting_svi_2_virtual(image, marked_image, len(text))
